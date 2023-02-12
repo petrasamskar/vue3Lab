293dfcbd162aef7e8a6db7e09cd6acbc8314ed29
Petra Sämskar <petra.samskar@gmail.com>
2023-02-12 18:36:45 +0100

    Added fetch

--- spaceX/src/components/Rockets.vue
+++ spaceX/src/components/Rockets.vue
@@ -1,14 +1,42 @@
 <template>
-    <Rocket />
+    <p>
+    <h1>SpaceX</h1>
+    <button @click="getAllRockets()">Get all rockets</button>
+    </p>
+    <div v-for="rocket in rockets">
+        <Rocket />
+    </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Rocket from './Rocket.vue';
+import { ref } from 'vue';
 
 
 export default defineComponent({
     name: "Rockets",
-    components: { Rocket }
+    components: { Rocket },
+    setup() {
+        const rockets = ref([])
+        const error = ref(null)
+        const getAllRockets = async () => {
+            try {
+                const data = await fetch('https://api.spacexdata.com/v3/rockets')
+                if (!data.ok) {
+                    throw Error('Something went wrong')
+                }
+                rockets.value = await data.json()
+
+            } catch (err) {
+                //error.value = err.message
+                console.log(err)
+            }
+        }
+        return {
+            rockets, error, getAllRockets
+        }
+
+    },
 })
 
 </script>
