18fc56badd60a6876c847d8d8a5f914e52c275df
Petra Sämskar <petra.samskar@gmail.com>
2023-02-15 12:18:14 +0100

    added link to wikipedia

--- spaceX/src/components/Rocket.vue
+++ spaceX/src/components/Rocket.vue
@@ -1,6 +1,8 @@
 <template>
     <h1>{{ rocket.rocket_name }}</h1>
     <p>{{ rocket.description }}</p>
+    <a v-bind:href="rocket.wikipedia" target=”_blank”>{{ rocket.wikipedia }}</a>
+
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
