c56c28ed7a42e3b78767e016b87b572dc30a6445
Petra Sämskar <petra.samskar@gmail.com>
2023-02-16 10:49:23 +0100

    temporary demo fix

--- spaceX/src/components/Rocket.vue
+++ spaceX/src/components/Rocket.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="card">
-    <div class="card-image">
-      <figure class="image is-4by3">
-        <img :src="rocket.flickr_images[0]" alt="No image available" />
-      </figure>
-    </div>
     <div class="card-content">
       <p class="title is-4">{{ rocket.rocket_name }}</p>
 
+      <figure class="image is-4by3">
+        <img :src="picture" alt="No image available" />
+      </figure>
+    </div>
+    <div class="card-content">
       <div class="content">
         {{ rocket.description }}
       </div>
@@ -20,7 +20,7 @@
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, computed } from "vue";
 import type { PropType } from "vue";
 import type { SpaceXRocket } from "../types/SpaceXRocket";
 
@@ -31,6 +31,20 @@ export default defineComponent({
       required: true,
       type: Object as PropType<SpaceXRocket>,
     },
+  },
+  setup(props) {
+    const picture = computed(() => {
+      // temporary for demo
+      if (props.rocket.flickr_images[0].includes("imgur")) {
+        return "https://live.staticflickr.com/65535/48953947006_313f01ec93_b.jpg";
+      } else {
+        return props.rocket.flickr_images[0];
+      }
+    });
+
+    return {
+      picture,
+    };
   },
 });
 </script>
